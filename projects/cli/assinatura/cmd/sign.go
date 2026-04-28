9d38fc7c30f0a064a05a3e523a06c891c20ade86
Caique Duarte <dcaique242@gmail.com>
2026-04-28 17:06:39 -0300

    feat: adiciona lógica para executar arquivo jar

--- projects/cli/assinatura/cmd/sign.go
+++ projects/cli/assinatura/cmd/sign.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 )
@@ -11,7 +13,25 @@ var signCmd = &cobra.Command{
 	Short: "Assina um documento",
 
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Assinando documento... wip...")
+		// Verifica e o arquivo .jar está
+		const JAR_PATH = "../../assinador/dist/assinador.jar"
+
+		if _, err := os.Stat(JAR_PATH); os.IsNotExist(err) {
+			fmt.Printf("Erro: O arquivo %s não foi encontrado.", JAR_PATH)
+			return
+		}
+
+		javaCmd := exec.Command("java", "-jar", JAR_PATH)
+
+		output, err := javaCmd.CombinedOutput()
+
+		if err != nil {
+			fmt.Printf("Erro ao executar o assinador: %s\n", err)
+			return
+		}
+
+		fmt.Println("Resultado do Assinador:")
+		fmt.Println(string(output))
 	},
 }
 
