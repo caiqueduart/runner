710923b2122e9e73f6e741ff90c5dfa930457f60
Caique Duarte <dcaique242@gmail.com>
2026-04-29 19:20:18 -0300

    wip: assinador cli

--- projects/assinador/src/Main.java
+++ projects/assinador/src/Main.java
@@ -1,11 +1,26 @@
 package src;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 
 public class Main {
-    public static void main(String[] args) {
+    // Definindo as cores como constantes para facilitar o uso
+    private static String _ANSI_RESET = "\u001B[0m";
+    private static String _ANSI_RED = "\u001B[31m";
+    private static String _ANSI_GREEN = "\u001B[32m";
+    private static String _ANSI_BLUE = "\u001B[34m";
+    private static String _ANSI_YELLOW = "\033[33m";
+
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        System.setOut(new PrintStream(System.out, true, "UTF-8"));
 
         String file = args[0];
 
         String assinaturaSimulada = "SIMULATED_SIG_" + file.hashCode();
-        System.out.println("Resultado: " + assinaturaSimulada);
+        System.out.println(
+            "\n\"" + _ANSI_GREEN + file + _ANSI_RESET + "\"" + 
+            " gerou código o de assinatura " 
+            + "\"" + _ANSI_GREEN + assinaturaSimulada + _ANSI_RESET + "\"."
+        );
+
     }
 }
--- projects/cli/assinatura/cmd/sign.go
+++ projects/cli/assinatura/cmd/sign.go
@@ -17,12 +17,19 @@ var signCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 
 		if file == "" {
-			fmt.Println("Você deve inserir um arquivo para asinatura!")
+			fmt.Println("Erro: Use a flag -f para indicar o caminho do arquivo.")
+			return
+		}
+
+		// 1. Validação do CLI: O arquivo do usuário existe?
+		content, err := os.ReadFile(file)
+		if err != nil {
+			fmt.Printf("Erro ao ler o arquivo '%s': %v\n", file, err)
 			return
 		}
 
 		const JAR_PATH = "../../assinador/src/Main.java"
-		var fullArgs = []string{ /* "-jar", */ JAR_PATH, file}
+		var fullArgs = []string{ /* "-jar", */ JAR_PATH, string(content)}
 
 		// Verifica se o arquivo .jar existe
 		if _, err := os.Stat(JAR_PATH); os.IsNotExist(err) {
