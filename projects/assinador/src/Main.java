4f2d9d8d1bfa86d4b339f8ac13f6da3a2fd52d0c
Caique Duarte <dcaique242@gmail.com>
2026-04-28 21:01:56 -0300

    wip: assinador cli

--- projects/assinador/src/Main.java
+++ projects/assinador/src/Main.java
@@ -2,10 +2,10 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\nAssinatura gerada com sucesso, argumentos:\n");
 
-        for(int i = 0; i < args.length; i++) {
-            System.out.println(i + " > " + args[i]);
-        }
+        String file = args[0];
+
+        String assinaturaSimulada = "SIMULATED_SIG_" + file.hashCode();
+        System.out.println("Resultado: " + assinaturaSimulada);
     }
 }
--- projects/cli/assinatura/cmd/sign.go
+++ projects/cli/assinatura/cmd/sign.go
@@ -8,14 +8,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var file string
+
 var signCmd = &cobra.Command{
 	Use:   "sign",
 	Short: "Assina um documento",
 
 	Run: func(cmd *cobra.Command, args []string) {
 
-		const JAR_PATH = "../../assinador/dist/assinador.jar"
-		var fullArgs = append([]string{"-jar", JAR_PATH}, args...)
+		if file == "" {
+			fmt.Println("Você deve inserir um arquivo para asinatura!")
+			return
+		}
+
+		const JAR_PATH = "../../assinador/src/Main.java"
+		var fullArgs = []string{ /* "-jar", */ JAR_PATH, file}
 
 		// Verifica se o arquivo .jar existe
 		if _, err := os.Stat(JAR_PATH); os.IsNotExist(err) {
@@ -37,5 +44,7 @@ var signCmd = &cobra.Command{
 }
 
 func init() {
+	signCmd.Flags().StringVarP(&file, "file", "f", "", "Arquivo para assinatura")
+
 	RootCmd.AddCommand(signCmd)
 }
