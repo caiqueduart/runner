c2fee38fff80bbd08753867650b3f07320f924e7
Caique Duarte <dcaique242@gmail.com>
2026-05-05 16:56:15 -0300

    feat: refatorações no código java

--- projects/assinador/src/Main.java
+++ projects/assinador/src/Main.java
@@ -1,15 +1,9 @@
 package src;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
+import src.services.Tint;
 
 public class Main {
-    // Definindo as cores como constantes para facilitar o uso
-    private static String _ANSI_RESET = "\u001B[0m";
-    private static String _ANSI_RED = "\u001B[31m";
-    private static String _ANSI_GREEN = "\u001B[32m";
-    private static String _ANSI_BLUE = "\u001B[34m";
-    private static String _ANSI_YELLOW = "\033[33m";
-
     public static void main(String[] args) throws UnsupportedEncodingException {
         System.setOut(new PrintStream(System.out, true, "UTF-8"));
 
@@ -17,10 +11,9 @@ public class Main {
 
         String assinaturaSimulada = "SIMULATED_SIG_" + file.hashCode();
         System.out.println(
-            "\n\"" + _ANSI_GREEN + file + _ANSI_RESET + "\"" + 
+            "\n\"" + Tint.GREEN + file + Tint.RESET + "\"" + 
             " gerou código o de assinatura " 
-            + "\"" + _ANSI_GREEN + assinaturaSimulada + _ANSI_RESET + "\"."
+            + "\"" + Tint.GREEN + assinaturaSimulada + Tint.RESET + "\"."
         );
-
     }
 }
---
+++ projects/assinador/src/services/Tint.java
@@ -0,0 +1,9 @@
+package src.services;
+
+public class Tint {
+    public static String RESET = "\u001B[0m";
+    public static String RED = "\u001B[31m";
+    public static String GREEN = "\u001B[32m";
+    public static String BLUE = "\u001B[34m";
+    public static String YELLOW = "\033[33m";   
+}
