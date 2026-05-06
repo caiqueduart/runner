729f90f416069ebc722237350b8dc52294e06aa3
Caique Duarte <dcaique242@gmail.com>
2026-05-05 22:31:15 -0300

    feat: melhorias no código da assinatura cli

--- projects/cli/assinatura/cmd/sign.go
+++ projects/cli/assinatura/cmd/sign.go
@@ -11,43 +11,58 @@ import (
 
 var file string
 
-const CMD_KEY = "sign"
+const (
+	CMD_KEY  = "sign"
+	JAR_PATH = "../../assinador/src/Main.java"
+)
 
 var signCmd = &cobra.Command{
 	Use:   CMD_KEY,
 	Short: "Assina um documento",
-
 	Run: func(cmd *cobra.Command, args []string) {
-
-		content, err := os.ReadFile(file)
-		if err != nil {
-			internal.PrintError("Ocorreu um erro ao ler o arquivo '%s': \n'%s'", file, err)
-			return
-		}
-
-		const JAR_PATH = "../../assinador/src/Main.java"
-		var fullArgs = []string{ /* "-jar", */ JAR_PATH, CMD_KEY, string(content)}
-
-		if _, err := os.Stat(JAR_PATH); os.IsNotExist(err) {
-			internal.PrintError("Erro: O arquivo '%s' não foi encontrado.", JAR_PATH)
-			return
-		}
-
-		javaCmd := exec.Command("java", fullArgs...)
-
-		output, err := javaCmd.CombinedOutput()
-
-		if err != nil {
-			internal.PrintError("Ocorreu um erro ao executar o assinador: \n'%s'", err)
-			return
-		}
-
-		fmt.Println(string(output))
+		runSign()
 	},
+}
+
+func runSign() {
+	// Validação de existência do assinador
+	if _, err := os.Stat(JAR_PATH); os.IsNotExist(err) {
+		internal.PrintError("Erro: O Assinador não foi encontrado em '%s'", JAR_PATH)
+		return
+	}
+
+	// Leitura do arquivo passado nos parâmetros
+	content, err := os.ReadFile(file)
+	if err != nil {
+		internal.PrintError("Erro ao ler o arquivo '%s': \n%v", file, err)
+		return
+	}
+
+	// Execução do comando externo
+	output, err := execJavaSigner(string(content))
+	if err != nil {
+		internal.PrintError("Erro ao executar o assinador: \n%v", err)
+		return
+	}
+
+	fmt.Println(output)
+}
+
+func execJavaSigner(payload string) (string, error) {
+	javaCmd := exec.Command("java", JAR_PATH, CMD_KEY, payload)
+
+	output, err := javaCmd.CombinedOutput()
+
+	if err != nil {
+		return "", fmt.Errorf("%w", err)
+	}
+
+	return string(output), nil
 }
 
 func init() {
 	signCmd.Flags().StringVarP(&file, "file", "f", "", "Arquivo para assinatura")
+	signCmd.MarkFlagRequired("file")
 
 	RootCmd.AddCommand(signCmd)
 }
