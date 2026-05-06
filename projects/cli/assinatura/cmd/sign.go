94bc0f517cbc905435600dc5c9e8851d0517a8e9
Caique Duarte <dcaique242@gmail.com>
2026-05-06 17:30:45 -0300

    feat: implementa o comando validate

--- projects/cli/assinatura/cmd/sign.go
+++ projects/cli/assinatura/cmd/sign.go
@@ -9,12 +9,12 @@ import (
 )
 
 var (
-	file   string
-	cmdKey = "sign"
+	sFile   string
+	sCmdKey = "sign"
 )
 
 var signCmd = &cobra.Command{
-	Use:   cmdKey,
+	Use:   sCmdKey,
 	Short: "Assina um documento",
 	Run: func(cmd *cobra.Command, args []string) {
 		runSign()
@@ -24,14 +24,14 @@ var signCmd = &cobra.Command{
 func runSign() {
 
 	// Leitura das informações do arquivo passado nos parâmetros
-	fileInfo, err := os.Stat(file)
+	fileInfo, err := os.Stat(sFile)
 	if err != nil {
-		internal.PrintError("Erro ao ler o arquivo '%s': \n%v", file, err)
+		internal.PrintError("Erro ao ler o arquivo '%s': \n%v", sFile, err)
 		return
 	}
 
 	// Execução do comando para o assinador
-	output, err := internal.ExecJavaSigner(string(fileInfo.Name()), cmdKey)
+	output, err := internal.ExecJavaSigner(string(fileInfo.Name()), sCmdKey)
 	if err != nil {
 		internal.PrintError("Erro ao executar o assinador: \n%v", err)
 		return
@@ -41,7 +41,7 @@ func runSign() {
 }
 
 func init() {
-	signCmd.Flags().StringVarP(&file, "file", "f", "", "Arquivo para assinatura")
+	signCmd.Flags().StringVarP(&sFile, "file", "f", "", "Arquivo para assinatura")
 	signCmd.MarkFlagRequired("file")
 
 	RootCmd.AddCommand(signCmd)
--- projects/cli/assinatura/cmd/validate.go
+++ projects/cli/assinatura/cmd/validate.go
@@ -2,19 +2,47 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"runner/assinatura/internal"
 
 	"github.com/spf13/cobra"
 )
 
-var validateCmd = &cobra.Command{
-	Use:   "validate",
-	Short: "Valida um documento",
+var (
+	vFile   string
+	vCmdKey = "validate"
+)
 
+var validateCmd = &cobra.Command{
+	Use:   vCmdKey,
+	Short: "Valida um documento",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Validando um documento... wip")
+		runValidate()
 	},
 }
 
+func runValidate() {
+
+	// Leitura das informações do arquivo passado nos parâmetros
+	fileInfo, err := os.Stat(vFile)
+	if err != nil {
+		internal.PrintError("Erro ao ler o arquivo '%s': \n%v", vFile, err)
+		return
+	}
+
+	// Execução do comando para o assinador
+	output, err := internal.ExecJavaSigner(string(fileInfo.Name()), vCmdKey)
+	if err != nil {
+		internal.PrintError("Erro ao executar o assinador: \n%v", err)
+		return
+	}
+
+	fmt.Println(output)
+}
+
 func init() {
+	validateCmd.Flags().StringVarP(&vFile, "file", "f", "", "Arquivo para assinatura")
+	validateCmd.MarkFlagRequired("file")
+
 	RootCmd.AddCommand(validateCmd)
 }
