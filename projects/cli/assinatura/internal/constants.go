bbc31e3abf209b33391b231f7451dfc87a0fc854
Caique Duarte <dcaique242@gmail.com>
2026-06-06 23:55:36 -0300

    fix: wip - correção de ci da assinatura

--- projects/cli/assinatura/internal/constants.go
+++ projects/cli/assinatura/internal/constants.go
@@ -1,7 +1,7 @@
 package internal
 
 const (
-	AssinaturaCLIVersion       = "1.1.2"
+	AssinaturaCLIVersion       = "1.1.3"
 	CompatibleAssinadorVersion = "1.0.4"
 	RepoPath                   = "caiqueduart/runner"
 	ServerPort                 = "8080"
