7519148bfdb32dafdf803a4ad6a42685c2bee1bd
Caique Duarte <dcaique242@gmail.com>
2026-06-17 22:09:29 -0300

    feat: versão final para avaliação de simulador e assinatura CLI

--- projects/cli/assinatura/internal/constants.go
+++ projects/cli/assinatura/internal/constants.go
@@ -1,7 +1,7 @@
 package internal
 
 const (
-	AssinaturaCLIVersion        = "1.2.0"
+	AssinaturaCLIVersion        = "1.2.1"
 	CompatibleAssinadorVersion  = "1.1.0"
 	RepoPath                    = "caiqueduart/runner"
 	DefaultServerPort           = 8080
