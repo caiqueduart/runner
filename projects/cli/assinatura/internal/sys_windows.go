c5d2413a9e369896eb0870219b226545ba7e92e2
Caique Duarte <dcaique242@gmail.com>
2026-06-17 21:32:20 -0300

    fix: corrige flags de criação de processo no Windows

--- projects/cli/assinatura/internal/sys_windows.go
+++ projects/cli/assinatura/internal/sys_windows.go
@@ -10,8 +10,8 @@ import (
 // configura o comando para rodar de forma independente no Windows.
 func SetDetachedProcess(cmd *exec.Cmd) {
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		CreationFlags: 0x01000000 | // CREATE_BREAKAWAY_FROM_JOB
-			0x00000008 | // DETACHED_PROCESS
+		CreationFlags: 0x00000008 | // DETACHED_PROCESS
 			0x00000200, // CREATE_NEW_PROCESS_GROUP
+		HideWindow: true,
 	}
 }
